f50a22cf44725d49a4e036e932eab62fc4c36432
vladislavbaimurzin <baimurzin.719@gmail.com>
2019-03-14 20:15:30 +0200

    courses

--- src/main/java/com/stdstack/service/gateway/zuul/GatewayZuulApplication.java
+++ src/main/java/com/stdstack/service/gateway/zuul/GatewayZuulApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableZuulProxy
@@ -17,6 +21,7 @@ public class GatewayZuulApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayZuulApplication.class, args);
     }
+
 
 }
 
--- src/main/java/com/stdstack/service/gateway/zuul/config/SecurityConfiguration.java
+++ src/main/java/com/stdstack/service/gateway/zuul/config/SecurityConfiguration.java
@@ -1,19 +1,27 @@
 package com.stdstack.service.gateway.zuul.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
 public class SecurityConfiguration extends ResourceServerConfigurerAdapter {
 
+
     @Override
     public void configure(final HttpSecurity http) throws Exception {
-        http.authorizeRequests().
+        http
+                .cors().and()
+                .authorizeRequests().
                 antMatchers("/oauth/**",
                         "/health",
                         "/courses/v2/api-docs",
@@ -24,8 +32,27 @@ public class SecurityConfiguration extends ResourceServerConfigurerAdapter {
                         "/webjars/**"
                         )
                 .permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .antMatchers("/**")
                 .authenticated();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
 }
